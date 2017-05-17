9ba298c3563cfcdf213f435b328454d58d2be948
stormlin <lin-jinitng@outlook.com>
2017-05-18 00:18:41 +0800

    Fixed some bugs and syntax errors. @2017/05/18 00:16.

--- src/FPGrowth.java
+++ src/FPGrowth.java
@@ -39,20 +39,24 @@ class FPGrowth {
     /**
      * 构建头项表，按递减排好序
      *
-     * @return
+     * @return 返回大于等于最小支持度的结点集合
      */
     private List<TNode> buildHeaderTable(List<List<String>> transactions) {
+        //todo: 确认此方法是否每次执行的结果都是一样的，如果一样的就可以抽出来，去除重复计算
         //大于最小支持度的节点 List
         List<TNode> headers = new ArrayList<>();
-        //节点名称和节点实体的键值对
+        //存储节点名称和节点实体的键值对
         Map<String, TNode> nodeMap = new HashMap<>();
 
-        //为每一个item构建一个节点
+        //note: 不要用记事本保存文件，里面会含有可恶的 BOM
+        //为所有在 transactions 中出现过的 item 都新增一个表项。
         for (List<String> line : transactions) {
             for (String itemName : line) {
                 if (nodeMap.keySet().contains(itemName)) {
+                    //如果此节点已经出现过，那么就给此节点的支持度加一
                     nodeMap.get(itemName).increaseCount(1);
                 } else {
+                    //如果还没有出现过这个 item，则在 map 中新增此节点
                     nodeMap.put(itemName, new TNode(itemName));
                 }
             }
@@ -83,7 +87,7 @@ class FPGrowth {
         //todo: 确认是否每一次都要 build 一次 HeaderTable
         List<TNode> headerTable = buildHeaderTable(transactions);
         //构建FP树
-        TNode tree = bulidFPTree(headerTable, transactions);
+        TNode tree = buildFPTree(headerTable, transactions);
         //当前行
         StringBuilder currentLine = new StringBuilder();
         //当树为空时退出
@@ -93,32 +97,32 @@ class FPGrowth {
         //输出频繁项集
         if (postPattern != null) {
             for (TNode head : headerTable) {
-                currentLine.delete(0, currentLine.length());
-//                System.out.print(head.getCount() + " " + head.getItemName());
-                currentLine.append(head.getCount()).append(" ").append(head.getItemName());
+                //currentLine.delete(0, currentLine.length());
+                System.out.print(head.getCount() + " " + head.getItemName());
+                //currentLine.append(head.getCount()).append(" ").append(head.getItemName());
                 for (String item : postPattern) {
-//                    System.out.print(" " + item);
-                    currentLine.append(" ").append(item);
+                    System.out.print(" " + item);
+                    //currentLine.append(" ").append(item);
                 }
-//                System.out.println();
-                result.add(currentLine);
+                System.out.println();
+                //result.add(currentLine);
             }
         }
         //遍历每一个头项表节点
         for (TNode head : headerTable) {
-            List<String> newPostPattern = new LinkedList<String>();
+            List<String> newPostPattern = new LinkedList<>();
             newPostPattern.add(head.getItemName());//添加本次模式基
             //加上将前面累积的前缀模式基
             if (postPattern != null) {
                 newPostPattern.addAll(postPattern);
             }
             //定义新的事务数据库
-            List<List<String>> newTransaction = new LinkedList<List<String>>();
+            List<List<String>> newTransaction = new LinkedList<>();
             TNode nextnode = head.getNext();
             //去除名称为head.getItemName()的模式基，构造新的事务数据库
             while (nextnode != null) {
                 int count = nextnode.getCount();
-                List<String> parentNodes = new ArrayList<String>();//nextnode节点的所有祖先节点
+                List<String> parentNodes = new ArrayList<>();//nextnode节点的所有祖先节点
                 TNode parent = nextnode.getParent();
                 while (parent.getItemName() != null) {
                     parentNodes.add(parent.getItemName());
@@ -137,15 +141,17 @@ class FPGrowth {
     }
 
     /**
-     * 构建FR-tree
+     * 构建FP-Tree
      *
-     * @param headertable 头项表
+     * @param headerTable 头项表
      * @return
      */
-    public TNode bulidFPTree(List<TNode> headertable, List<List<String>> transactions) {
+    private TNode buildFPTree(List<TNode> headerTable, List<List<String>> transactions) {
+        //todo: 确认此方法的执行结果是否唯一
+        //FP 树根节点
         TNode rootNode = new TNode();
         for (List<String> items : transactions) {
-            LinkedList<String> itemsDesc = sortItemsByDesc(items, headertable);
+            LinkedList<String> itemsDesc = sortItemsByDesc(items, headerTable);
             //寻找添加itemsDesc为子树的父节点
             TNode subtreeRoot = rootNode;
             if (subtreeRoot.getChildren().size() != 0) {
@@ -158,7 +164,7 @@ class FPGrowth {
                 }
             }
             //将itemsDesc中剩余的节点加入作为subtreeRoot的子树
-            addSubTree(headertable, subtreeRoot, itemsDesc);
+            addSubTree(headerTable, subtreeRoot, itemsDesc);
         }
         return rootNode;
     }
@@ -168,7 +174,7 @@ class FPGrowth {
      * @param subtreeRoot 子树父节点
      * @param itemsDesc   被添加的子树
      */
-    public void addSubTree(List<TNode> headertable, TNode subtreeRoot, LinkedList<String> itemsDesc) {
+    private void addSubTree(List<TNode> headertable, TNode subtreeRoot, LinkedList<String> itemsDesc) {
         if (itemsDesc.size() > 0) {
             TNode thisnode = new TNode(itemsDesc.pop());//构建新节点
             subtreeRoot.getChildren().add(thisnode);
@@ -189,14 +195,24 @@ class FPGrowth {
         }
     }
 
-    //将items按count从高到低排序
-    public LinkedList<String> sortItemsByDesc(List<String> items, List<TNode> headertable) {
-        LinkedList<String> itemsDesc = new LinkedList<String>();
-        for (TNode node : headertable) {
+    /**
+     * 把 items 按照降序排列
+     *
+     * @param items 需要排序的输入
+     * @param headerTable 每一项的列表
+     * @return 已排序的结果
+     */
+    private LinkedList<String> sortItemsByDesc(List<String> items, List<TNode> headerTable) {
+        //以降序排列的 items
+        LinkedList<String> itemsDesc = new LinkedList<>();
+
+        //
+        for (TNode node : headerTable) {
             if (items.contains(node.getItemName())) {
                 itemsDesc.add(node.getItemName());
             }
         }
+
         return itemsDesc;
     }
 
@@ -213,13 +229,13 @@ class FPGrowth {
         private TNode next;//下一个同名节点
 
         public TNode() {
-            this.children = new ArrayList<TNode>();
+            this.children = new ArrayList<>();
         }
 
         public TNode(String name) {
             this.itemName = name;
             this.count = 1;
-            this.children = new ArrayList<TNode>();
+            this.children = new ArrayList<>();
         }
 
         public TNode findChildren(String childName) {
@@ -266,6 +282,10 @@ class FPGrowth {
         @Override
         public int compareTo(TNode o) {
             return o.getCount() - this.getCount();
+        }
+
+        public String toString(){
+            return String.valueOf(this.count);
         }
     }
 }
