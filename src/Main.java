a167c0909bd69da62ec9da32771a000c206d31c5
stormlin <lin-jinitng@outlook.com>
2017-05-18 21:29:46 +0800

    Commit for retrospect. @2017/05/18 18:04.

--- src/Main.java
+++ src/Main.java
@@ -28,7 +28,7 @@ public class Main {
         int sheetIndex = 0;
 
         //分隔符
-        String separator = " ";
+        String separator = ",";
 
         //频繁项中间结果
         List<StringBuilder> result = new ArrayList<>();
@@ -40,58 +40,58 @@ public class Main {
         预处理
          */
         //读取 CSV 文件，构建原始输入
-//        File csvFile = new File(csvFilePath);
-//
-//        if (!csvFile.exists() || !csvFile.isFile()) {
-//            System.out.println("无法读取指定文件，程序退出");
-//            return;
-//        }
-//
-//        //按文本文件的方式读取 CSV 文件
-//        System.out.println("正在读取 CSV 文件");
-//        try {
-//            String line;
-//            BufferedReader reader = new BufferedReader(new FileReader(txtFilePath));
-//            while ((line = reader.readLine()) != null) {
-//                csvInput.add(Arrays.asList(line.split(separator)));
-//            }
-//        } catch (Exception e) {
-//            e.printStackTrace();
-//            return;
-//        }
-//
-//        //去除只有一项的行，然后合并属于同一次借书操作的连续几行记录
-//        List<String> merged = new ArrayList<>();
-//        List<List<String>> FPInput = new ArrayList<>();
-//        int i;
-//        int j;
-//        int inputItemCount = csvInput.size();
-//
-//        System.out.println("CSV 文件读取完毕，开始预处理过程");
-//
-//        //由于以二进制方式读取的数据会含有标题行，故下标从 1 开始
-//        for (i = 1; i < inputItemCount; i++) {
-//            merged.add(csvInput.get(i).get(1));
-//            for (j = i; j < inputItemCount - 1; j++) {
-//                if (Objects.equals(csvInput.get(j).get(0), csvInput.get(j + 1).get(0))) {
-//                    merged.add(csvInput.get(j + 1).get(1));
-//                } else {
-//                    break;
-//                }
-//            }
-//            i = j;
-//            if (merged.size() == 1) {
-//                merged.clear();
-//            } else {
-//                Collections.sort(merged);
-//                //note: add() 是否是引用式的添加，还是复制式的添加？
-//                FPInput.add(new ArrayList<>(merged));
-//                merged.clear();
-//            }
-//        }
+        File csvFile = new File(csvFilePath);
 
-//        System.out.println("预处理结束，开始 FP-Growth 过程");
-//
+        if (!csvFile.exists() || !csvFile.isFile()) {
+            System.out.println("无法读取指定文件，程序退出");
+            return;
+        }
+
+        //按文本文件的方式读取 CSV 文件
+        System.out.println("正在读取 CSV 文件");
+        try {
+            String line;
+            BufferedReader reader = new BufferedReader(new FileReader(csvFile));
+            while ((line = reader.readLine()) != null) {
+                csvInput.add(Arrays.asList(line.split(separator)));
+            }
+        } catch (Exception e) {
+            e.printStackTrace();
+            return;
+        }
+
+        //去除只有一项的行，然后合并属于同一次借书操作的连续几行记录
+        List<String> merged = new ArrayList<>();
+        List<List<String>> FPInput = new ArrayList<>();
+        int i;
+        int j;
+        int inputItemCount = csvInput.size();
+
+        System.out.println("CSV 文件读取完毕，开始预处理过程");
+
+        //由于以二进制方式读取的数据会含有标题行，故下标从 1 开始
+        for (i = 1; i < inputItemCount; i++) {
+            merged.add(csvInput.get(i).get(1));
+            for (j = i; j < inputItemCount - 1; j++) {
+                if (Objects.equals(csvInput.get(j).get(0), csvInput.get(j + 1).get(0))) {
+                    merged.add(csvInput.get(j + 1).get(1));
+                } else {
+                    break;
+                }
+            }
+            i = j;
+            if (merged.size() == 1) {
+                merged.clear();
+            } else {
+                Collections.sort(merged);
+                //note: add() 是否是引用式的添加，还是复制式的添加？
+                FPInput.add(new ArrayList<>(merged));
+                merged.clear();
+            }
+        }
+
+        System.out.println("预处理结束，开始 FP-Growth 过程");
+
 //        for (List<String> row :
 //                FPInput) {
 //            for (String item :
@@ -101,26 +101,24 @@ public class Main {
 //            System.out.println();
 //        }
 
-//        String outputFilePath = "D:\\output.txt";
-//        File outputFile = new File(outputFilePath);
-//        try {
-//            outputFile.createNewFile();
-//            BufferedWriter writer = new BufferedWriter(new FileWriter(outputFile));
-//
-//            for (List<String> row :
-//                    FPInput) {
-//                for (String item :
-//                        row) {
-//                    writer.write(item + " ");
-//                }
-//                writer.write("\n");
-//            }
-//
-//            writer.flush();
-//            writer.close();
-//        } catch (IOException e) {
-//            e.printStackTrace();
-//        }
+        String outputFilePath = "D:\\output.txt";
+        File outputFile = new File(outputFilePath);
+        try {
+            outputFile.createNewFile();
+            BufferedWriter writer = new BufferedWriter(new FileWriter(outputFile));
+
+            for (List<String> row : FPInput) {
+                for (String item : row) {
+                    writer.write(item + " ");
+                }
+                writer.write("\n");
+            }
+
+            writer.flush();
+            writer.close();
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
 
 
 
@@ -128,7 +126,7 @@ public class Main {
         FP-Growth 算法处理
          */
         FPGrowth fpGrowth = new FPGrowth(2);
-        List<List<String>> transactions = fpGrowth.loadTransactions("D:\\output.txt", separator);
+        List<List<String>> transactions = fpGrowth.loadTransactions("D:\\test.txt", " ");
         fpGrowth.FPGrowth(transactions, null, result);
         //fpGrowth.FPGrowth(FPInput, null, result);
 
