19f46057b2c109cdd75bdf099c72bb98338bfdd2
stormlin <lin-jinitng@outlook.com>
2017-05-19 00:27:54 +0800

    Finished book name dictionary construct method. @2017/05/19 00:27.

--- src/Main.java
+++ src/Main.java
@@ -1,6 +1,4 @@
-import javax.swing.*;
 import java.io.*;
-import java.lang.reflect.Array;
 import java.util.*;
 
 /**
@@ -13,6 +11,7 @@ import java.util.*;
 
 public class Main {
 
+    //todo: 改写为 GUI 程序
     public static void main(String[] args) {
 
         //默认文本输入文件位置
@@ -22,7 +21,7 @@ public class Main {
         String csvFilePath = "D:\\test.csv";
 
         //默认书名字典位置
-        String dictionaryFilePath = "D:\\dictionary.xlsx";
+        String dictionaryFilePath = "D:\\dictionary.csv";
 
         //数据在 Excel 文件中的第几个 sheet，从 0 开始
         int sheetIndex = 0;
@@ -92,40 +91,31 @@ public class Main {
 
         System.out.println("预处理结束，开始 FP-Growth 过程");
 
-//        for (List<String> row :
-//                FPInput) {
-//            for (String item :
-//                    row) {
-//                System.out.print(item + " ");
+//        String outputFilePath = "D:\\output.txt";
+//        File outputFile = new File(outputFilePath);
+//        try {
+//            outputFile.createNewFile();
+//            BufferedWriter writer = new BufferedWriter(new FileWriter(outputFile));
+//
+//            for (List<String> row : FPInput) {
+//                for (String item : row) {
+//                    writer.write(item + " ");
+//                }
+//                writer.write("\n");
 //            }
-//            System.out.println();
+//
+//            writer.flush();
+//            writer.close();
+//        } catch (IOException e) {
+//            e.printStackTrace();
 //        }
-
-        String outputFilePath = "D:\\output.txt";
-        File outputFile = new File(outputFilePath);
-        try {
-            outputFile.createNewFile();
-            BufferedWriter writer = new BufferedWriter(new FileWriter(outputFile));
-
-            for (List<String> row : FPInput) {
-                for (String item : row) {
-                    writer.write(item + " ");
-                }
-                writer.write("\n");
-            }
-
-            writer.flush();
-            writer.close();
-        } catch (IOException e) {
-            e.printStackTrace();
-        }
 
 
 
         /*
         FP-Growth 算法处理
          */
-        FPGrowth fpGrowth = new FPGrowth(2);
+        FPGrowth fpGrowth = new FPGrowth(4);
         List<List<String>> transactions = fpGrowth.loadTransactions("D:\\test.txt", " ");
         fpGrowth.FPGrowth(transactions, null, result);
         //fpGrowth.FPGrowth(FPInput, null, result);
@@ -133,10 +123,32 @@ public class Main {
         /*
         再处理
          */
+        //制作字典
+        Dictionary<String, String> dictionary = new Hashtable<>();
 
-        System.out.println("处理完成，开始输出结果");
+        try {
+            String line;
+            BufferedReader reader = new BufferedReader(new FileReader(dictionaryFilePath));
+            while ((line = reader.readLine()) != null) {
+                List<String> dictionaryLine = Arrays.asList(line.split(","));
+                if (dictionaryLine.size() == 2) {
+                    dictionary.put(dictionaryLine.get(0), dictionaryLine.get(1));
+                }
+            }
+        } catch (Exception e) {
+            e.printStackTrace();
+            return;
+        }
+        //以文件流的形式读取 CSV 文件时，会把标题行读出来，所以需要去除
+        dictionary.remove("图书记录号（种）");
+
+        //去除前缀以及逆序后的结果
+        List<List<String>> reverse = new ArrayList<>();
+
+
 
         //结果输出
+        System.out.println("处理完成，开始输出结果");
         if (result.size() != 0) {
             for (StringBuilder row :
                     result) {
@@ -148,76 +160,6 @@ public class Main {
 
         System.out.println("输出完成，程序结束");
 
-//        // 创建 JFrame 实例
-//        JFrame frame = new JFrame("Login Example");
-//        // Setting the width and height of frame
-//        frame.setSize(350, 200);
-//        frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
-//
-//        /* 创建面板，这个类似于 HTML 的 div 标签
-//         * 我们可以创建多个面板并在 JFrame 中指定位置
-//         * 面板中我们可以添加文本字段，按钮及其他组件。
-//         */
-//        JPanel panel = new JPanel();
-//        // 添加面板
-//        frame.add(panel);
-//        /*
-//         * 调用用户定义的方法并添加组件到面板
-//         */
-//        placeComponents(panel);
-//
-//        // 设置界面可见
-//        frame.setVisible(true);
-
-//        //todo: 改写为 GUI 窗口程序
-//        JFileChooser fd = new JFileChooser();
-////fd.setFileSelectionMode(JFileChooser.DIRECTORIES_ONLY);
-//        fd.showDialog(new JLabel(), "选择文件");
-//        File f = fd.getSelectedFile();
-//        if(f != null){}
-
-    }
-
-    private static void placeComponents(JPanel panel) {
-
-        /* 布局部分我们这边不多做介绍
-         * 这边设置布局为 null
-         */
-        panel.setLayout(null);
-
-        // 创建 JLabel
-        JLabel userLabel = new JLabel("User:");
-        /* 这个方法定义了组件的位置。
-         * setBounds(x, y, width, height)
-         * x 和 y 指定左上角的新位置，由 width 和 height 指定新的大小。
-         */
-        userLabel.setBounds(10, 20, 80, 25);
-        panel.add(userLabel);
-
-        /*
-         * 创建文本域用于用户输入
-         */
-        JTextField userText = new JTextField(20);
-        userText.setBounds(100, 20, 165, 25);
-        panel.add(userText);
-
-        // 输入密码的文本域
-        JLabel passwordLabel = new JLabel("Password:");
-        passwordLabel.setBounds(10, 50, 80, 25);
-        panel.add(passwordLabel);
-
-        /*
-         *这个类似用于输入的文本域
-         * 但是输入的信息会以点号代替，用于包含密码的安全性
-         */
-        JPasswordField passwordText = new JPasswordField(20);
-        passwordText.setBounds(100, 50, 165, 25);
-        panel.add(passwordText);
-
-        // 创建登录按钮
-        JButton loginButton = new JButton("login");
-        loginButton.setBounds(10, 80, 80, 25);
-        panel.add(loginButton);
     }
 
 }
